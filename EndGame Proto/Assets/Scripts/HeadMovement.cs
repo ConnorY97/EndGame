969d12a6a205b2abf462c3e8a139d58c8108c13a
Connor <c_young97@live.com>
2020-06-23 13:25:05 +1000

    Working on movement

---
+++ EndGame Proto/Assets/Scripts/HeadMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMovement : MonoBehaviour
+{
+	public float speed = 10.0f;
+	public float maxSpeed = 100.0f; 
+	private Rigidbody rb;
+
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>(); 
+	}
+
+	private void FixedUpdate()
+	{
+		float movementHorizontal = Input.GetAxis("Horizontal");
+		float movementVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+		rb.AddForce(movement * speed);
+
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed); 
+
+		Debug.Log(rb.velocity.x); 
+
+		//if (Input.GetKey(KeyCode.D))
+		//{
+		//	transform.position += Vector3.right * speed * Time.deltaTime;
+		//}
+		//if (Input.GetKey(KeyCode.A))
+		//{
+		//	transform.position += Vector3.left * speed * Time.deltaTime;
+		//}
+		//if (Input.GetKey(KeyCode.W))
+		//{
+		//	transform.position += Vector3.forward * speed * Time.deltaTime;
+		//}
+		//if (Input.GetKey(KeyCode.S))
+		//{
+		//	transform.position += Vector3.back * speed * Time.deltaTime;
+		//}
+	}
+}
