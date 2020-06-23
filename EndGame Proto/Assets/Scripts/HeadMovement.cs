7dbc1b9eba89954aa914fcfbfb5d222dd8285d36
Connor <c_young97@live.com>
2020-06-23 15:32:33 +1000

    Working on Character

--- EndGame Proto/Assets/Scripts/HeadMovement.cs
+++ EndGame Proto/Assets/Scripts/HeadMovement.cs
@@ -2,19 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State
+{
+	HEAD,
+	ARMS,
+	LEGS,
+	FULL
+}
+
 public class HeadMovement : MonoBehaviour
 {
 	public float speed = 10.0f;
 	public float maxSpeed = 100.0f; 
 	private Rigidbody rb;
 
+	private State currentState;
+
+	public GameObject arms;
+	public GameObject legs;
+
+	public SphereCollider headCollider;
+	public BoxCollider bodyCollider;
+	public CapsuleCollider armsCollider; 
+
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody>(); 
 	}
 
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))		
+			currentState = State.HEAD;
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+			currentState = State.ARMS;
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+			currentState = State.LEGS;
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+			currentState = State.FULL; 
+	}
+
 	private void FixedUpdate()
 	{
+
+		//Depending on the state of the body will decide which method of movement is used 
+		switch (currentState)
+		{
+			case State.HEAD:
+				arms.SetActive(false);
+				legs.SetActive(false);
+				bodyCollider.enabled = false;
+				armsCollider.enabled = false;
+				break;
+			case State.ARMS:
+				arms.SetActive(true);
+				legs.SetActive(false);
+				bodyCollider.enabled = false;
+				armsCollider.enabled = true;
+				break;
+			case State.LEGS:
+				arms.SetActive(false);
+				legs.SetActive(true);
+				bodyCollider.enabled = true;
+				armsCollider.enabled = false;
+				break;
+			case State.FULL:
+				arms.SetActive(true);
+				legs.SetActive(true);
+				bodyCollider.enabled = true;
+				armsCollider.enabled = false;
+				break;
+			default:
+				break;
+		}
+
 		float movementHorizontal = Input.GetAxis("Horizontal");
 		float movementVertical = Input.GetAxis("Vertical");
 
@@ -22,25 +83,8 @@ public class HeadMovement : MonoBehaviour
 
 		rb.AddForce(movement * speed);
 
-		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed); 
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
 
-		Debug.Log(rb.velocity.x); 
-
-		//if (Input.GetKey(KeyCode.D))
-		//{
-		//	transform.position += Vector3.right * speed * Time.deltaTime;
-		//}
-		//if (Input.GetKey(KeyCode.A))
-		//{
-		//	transform.position += Vector3.left * speed * Time.deltaTime;
-		//}
-		//if (Input.GetKey(KeyCode.W))
-		//{
-		//	transform.position += Vector3.forward * speed * Time.deltaTime;
-		//}
-		//if (Input.GetKey(KeyCode.S))
-		//{
-		//	transform.position += Vector3.back * speed * Time.deltaTime;
-		//}
+		//Debug.Log(rb.velocity.x);
 	}
 }
