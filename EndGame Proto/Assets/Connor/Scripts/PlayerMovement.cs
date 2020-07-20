009d9d9e724dd4008d5085b21afe86095b5cf8a2
Connor <c_young97@live.com>
2020-07-20 17:13:14 +1000

    Got some weird bugs going on lol

--- EndGame Proto/Assets/Connor/Scripts/PlayerMovement.cs
+++ EndGame Proto/Assets/Connor/Scripts/PlayerMovement.cs
@@ -13,23 +13,23 @@ public enum STATE
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField] private float _speed;
-	[SerializeField] private float _maxSpeed = 100.0f;
 	[SerializeField] private float _angularSpeed;
 	private Rigidbody _rb;
 	private bool _holding = false;
+	private bool _lifting = false; 
 
 	private STATE _currentState = STATE.FREE; 
 
 	private Transform _cameraTransform;
+	private Vector3 _heading; 
 	private Vector3 _forward;
 	private Vector3 _right;
-	private Vector2 _input;
-	private Vector2 _inputNormalized;
+	private Vector3 _input;
 	private float _angle;
 
-	private Vector3 _mosuePos;
-	//Transform _trans;
-	private Vector3 _objPos;
+	//private Vector3 _mosuePos;
+	////Transform _trans;
+	//private Vector3 _objPos;
 
 	private Rigidbody _heldObject;
 	private Vector3 _heldObjectNormal;
@@ -48,17 +48,28 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		_mosuePos = Input.mousePosition;
-		_mosuePos.z = Vector3.Distance(transform.position, Camera.main.transform.position); //The distance between the camera and object
-		_objPos = Camera.main.WorldToScreenPoint(transform.position);
-		_mosuePos.x = _mosuePos.x - _objPos.x;
-		_mosuePos.y = _mosuePos.y - _objPos.y;
-		_angle = Mathf.Atan2(_mosuePos.y, _mosuePos.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(new Vector3(0, 90 - _angle, 0));
+		float movementHorizontal = Input.GetAxis("Horizontal");
+		float movementVertical = Input.GetAxis("Vertical");
+
+		_input = new Vector3(movementHorizontal, 0.0f, movementVertical);
+		//_mosuePos = Input.mousePosition;
+		//_mosuePos.z = Vector3.Distance(transform.position, Camera.main.transform.position); //The distance between the camera and object
+		//_objPos = Camera.main.WorldToScreenPoint(transform.position);
+		//_mosuePos.x = _mosuePos.x - _objPos.x;
+		//_mosuePos.y = _mosuePos.y - _objPos.y;
+		//_angle = Mathf.Atan2(_mosuePos.y, _mosuePos.x) * Mathf.Rad2Deg;
+		//transform.rotation = Quaternion.Euler(new Vector3(0, 90 - _angle, 0));
+
+		_angle = _cameraTransform.rotation.eulerAngles.y;
+		_forward = Quaternion.AngleAxis(_angle, Vector3.up) * Vector3.forward;
+		_right = Vector3.Cross(Vector3.up, _forward);
+
+		_heading = _input.normalized.x * _right + _input.normalized.z * _forward;
+
+		Quaternion targetRotation = Quaternion.LookRotation(_forward, Vector3.up);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _angularSpeed * Time.fixedDeltaTime);
 
 		RaycastHit hit;
-
-		Debug.DrawRay(transform.position - new Vector3(0, 0.05f, 0), transform.forward * 2, Color.white);
 
 		//Pushing 
 		if (Input.GetKeyDown(KeyCode.E))
@@ -70,7 +81,6 @@ public class PlayerMovement : MonoBehaviour
 					_heldObject = hit.collider.GetComponent<Rigidbody>();
 					_heldObject.isKinematic = false;
 					_heldObjectNormal = hit.normal;
-					//hit.collider.GetComponent<Rigidbody>().velocity = this.GetComponent<Rigidbody>().velocity;
 					_currentState = STATE.PUSHING;
 					_holding = true;
 				}
@@ -85,7 +95,7 @@ public class PlayerMovement : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Q))
 		{
-			if (_holding == false)
+			if (_lifting == false)
 			{
 				if (Physics.Raycast(transform.position - new Vector3(0, 0.5f, 0), transform.forward, out hit, 2, _layerMask))
 				{
@@ -94,12 +104,12 @@ public class PlayerMovement : MonoBehaviour
 					_heldOjectPosition = _heldObject.GetComponent<Transform>().position;
 					_heldObject.GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z); 
 					_currentState = STATE.LIFTING;
-					_holding = true;
+					_lifting = true;
 				}
 			}
-			else if (_holding == true)
+			else if (_lifting == true)
 			{
-				_holding = false;
+				_lifting = false;
 				_heldObject.isKinematic = false;
 				_heldObject.GetComponent<Transform>().position = _heldOjectPosition;
 				_currentState = STATE.FREE; 
@@ -113,25 +123,18 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-		float movementHorizontal = Input.GetAxis("Horizontal");
-		float movementVertical = Input.GetAxis("Vertical");
-
-		Vector3 input = new Vector3(movementHorizontal, 0.0f, movementVertical);
-
-
-
 		switch(_currentState)
 		{
 			case STATE.FREE:
 				{
-					_rb.constraints = RigidbodyConstraints.None; 
-					_rb.velocity = input.normalized * _speed; 
+					_rb.constraints = RigidbodyConstraints.FreezeRotation; 
+					_rb.velocity = _heading * _speed; 
 					break; 
 				}
 			case STATE.PUSHING:
 				{
+					_rb.velocity = _input.z * -_heldObjectNormal * _speed;
 					_heldObject.velocity = _rb.velocity; 
-					_rb.velocity = input.z * -_heldObjectNormal * _speed;
 					_rb.constraints = RigidbodyConstraints.FreezeRotation; 
 					break;
 				}
@@ -141,50 +144,6 @@ public class PlayerMovement : MonoBehaviour
 					break; 
 				}
 		}
-
-
-		//if (Input.GetKey(KeyCode.E))
-		//{
-		//	if (Physics.Raycast(transform.position - new Vector3(0, 0.5f, 0), transform.TransformDirection(Vector3.forward), out hit, 5, _layerMask))
-		//	{
-		//		//Debug.DrawRay(transform.position - new Vector3(0, 0.5f, 0), transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-
-		//		_rb.velocity = -hit.normal * movement.z * _speed;
-		//	}
-		//	else
-		//	{
-		//		//Debug.DrawRay(transform.position - new Vector3(0, 0.5f, 0), transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-		//		Debug.Log("Did not Hit");
-		//	}
-		//}
-
-		//if (Input.GetKeyDown(KeyCode.Q))
-		//{
-		//	if (Physics.Raycast(transform.position - new Vector3(0, 0.5f, 0), transform.TransformDirection(Vector3.forward), out hit, 5, _layerMask))
-		//	{
-		//		if (!_lifting)
-		//		{
-		//			_liftedObject = hit.collider.GetComponent<Transform>();
-		//			_originalPosition = _liftedObject.position; 
-		//			_liftedObject.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-		//			_liftedObject.GetComponent<Rigidbody>().isKinematic = false;
-		//			_rb.constraints = RigidbodyConstraints.FreezeAll;
-		//			_lifting = true;
-		//		}
-		//	}
-		//	else if (_lifting)
-		//	{
-		//		_liftedObject.position = _originalPosition; 
-		//		_liftedObject.GetComponent<Rigidbody>().isKinematic = true;
-		//		_rb.constraints = RigidbodyConstraints.None;
-		//		_lifting = false;
-		//	}
-		//}
-
-
-
-		//if (!_holding)
-		//	_rb.velocity = input.normalized * _speed;
 	}
 
 	
