c440a751a0eda442a182634744e88692bb7db9b8
dyfust <dyfust@gmail.com>
2020-07-27 10:25:40 +1000

    fixed trivial errors

--- EndGame Proto/Assets/Rey/Scripts/Player/Golem.cs
+++ EndGame Proto/Assets/Rey/Scripts/Player/Golem.cs
@@ -41,8 +41,7 @@ public class Golem : MonoBehaviour, IRequireInput
 
     private void FixedUpdate()
     {
-        if (_currentState == STATE.ACTIVE)
-            Orientate();
+        Orientate();
     }
 
     private void InitialiseFSM()
